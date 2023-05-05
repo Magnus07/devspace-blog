af2625e3134966abfaa648acf4e997267c22ab0a
Magnus07 <stanislav.pinchuk.02@gmail.com>
2023-05-05 12:39:33 +0300

    added frontmatter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 import Layout from "@/components/Layout";
 
 export default function HomePage() {
@@ -6,4 +9,25 @@ export default function HomePage() {
       <h1>Hello World</h1>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("src/posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("src/posts", filename),
+      "utf-8"
+    );
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return { slug, frontmatter };
+  });
+
+  console.log(posts);
+  return {
+    props: {},
+  };
 }
