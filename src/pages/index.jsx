34c7da13dbecbbcfea224899d74e4caf487d4ec9
Magnus07 <stanislav.pinchuk.02@gmail.com>
2023-05-06 12:51:02 +0300

    added posts to main page

---
+++ src/pages/index.jsx
@@ -0,0 +1,46 @@
+import fs from "fs";
+import path from "path";
+import Link from "next/link";
+import matter from "gray-matter";
+import { sortByDate } from "@/utils";
+import Post from "@/components/Post";
+import Layout from "@/components/Layout";
+
+export default function HomePage({ posts }) {
+  return (
+    <Layout>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Latest Posts</h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+      <Link href="/blog" legacyBehavior>
+        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
+          All Posts
+        </a>
+      </Link>
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync(path.join("src/posts"));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace(".md", "");
+
+    const markdownWithMeta = fs.readFileSync(
+      path.join("src/posts", filename),
+      "utf-8"
+    );
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return { slug, frontmatter };
+  });
+
+  return {
+    props: { posts: posts.sort(sortByDate).slice(0, 6) },
+  };
+}
