ca766306c5db00e604789e0cb8017725a059f951
Magnus07 <stanislav.pinchuk.02@gmail.com>
2023-05-09 14:19:05 +0300

    adds categories sidebar

--- src/pages/blog/category/[category_name].jsx
+++ src/pages/blog/category/[category_name].jsx
@@ -4,18 +4,25 @@ import matter from "gray-matter";
 import Post from "@/components/Post";
 import Layout from "@/components/Layout";
 import { getPosts } from "@/lib/posts";
+import CategoryList from "@/components/CategoryList";
 
-export default function CategoryBlogPage({ posts, categoryName }) {
+export default function CategoryBlogPage({ posts, categoryName, categories }) {
   return (
     <Layout>
-      <h1 className="text-5xl border-b-4 p-5 font-bold">
-        Posts in {categoryName}
-      </h1>
-
-      <div className="grid md:grid-cols-2 lg:grid-cols-3">
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+      <div className="flex justify-between">
+        <div className="w-3/4 mr-10">
+          <h1 className="text-5xl border-b-4 p-5 font-bold">
+            Posts in {categoryName}
+          </h1>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3">
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+        </div>
+        <div className="w-1/4">
+          <CategoryList categories={categories} />
+        </div>
       </div>
     </Layout>
   );
@@ -50,10 +57,15 @@ export async function getStaticProps({ params: { category_name } }) {
     (post) => post.frontmatter.category.toLowerCase() === category_name
   );
 
+  // Get categories for sidebar
+  const categories = posts.map((post) => post.frontmatter.category);
+  const uniqueCategories = [...new Set(categories)];
+
   return {
     props: {
       posts: categoryPosts,
       categoryName: category_name,
+      categories: uniqueCategories,
     },
   };
 }
