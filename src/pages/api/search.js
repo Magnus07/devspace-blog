3d75965d21ba23a25df834c28bf249bf5b9b60d0
Magnus07 <stanislav.pinchuk.02@gmail.com>
2023-05-10 11:59:01 +0300

    adds search component

---
+++ src/pages/api/search.js
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { getPosts } from "@/lib/posts";
+
+export default function handler(req, res) {
+  let posts;
+
+  if (process.env.NODE_ENV === "production") {
+    // @todo - fetch from cache
+  } else {
+    posts = getPosts();
+  }
+
+  const results = posts.filter(
+    ({ frontmatter: { title, excerpt, category } }) =>
+      title.toLowerCase().indexOf(req.query.q) != -1 ||
+      excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
+      category.toLowerCase().indexOf(req.query.q) != -1
+  );
+
+  res.status(200).json(JSON.stringify({ results }));
+}
